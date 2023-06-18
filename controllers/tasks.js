1cb56c4e01097f7daa3522c28113a62a0062600a
mrtechnostart <rambpandey238@gmail.com>
2023-06-18 22:26:37 +0530

    Add database schema, task.js update

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,17 @@
+const Addr = require("../models/address")
+
 const postDetails = async(req,res)=>{
-    res.json({
-        success:"true"
-    })
+    try {
+        const data = req.body
+        const task = await Addr.create(data)
+        res.status(201).json(task);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+const getRequest = async(req,res)=>{
+    
 }
 
 module.exports = {
